c5d964c8b82e16e1d9b473a69bf13bfd95d9f39f
kvarad6 <kvarad6@gmail.com>
2023-10-17 15:55:53 +0530

    fixed uri

--- server/index.js
+++ server/index.js
@@ -10,7 +10,7 @@ app.use(cors())
 app.use(express.json())
 
 
-mongoose.connect('mongodb+srv://root:root@cluster0.pmm1cfh.mongodb.net/user-database')
+mongoose.connect('')
     .then(() => console.log('Connected!'));
 
 
